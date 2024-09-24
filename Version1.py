ddc13547be90c35662d3a1cbf3aec0275a5166bd
PlayLancePlay <playlanceplay@gmail.com>
2024-09-23 19:03:04 -0700

    Add files via upload

---
+++ Version1.py
@@ -0,0 +1,8 @@
+# Version 1
+# Stage 1
+num_showers = int(input)("Enter the number of showers you take per week: ")
+print("I take " + num_showers + " showers per week") # I take 7 showers per wekk
+avg_per_showers = int(input)("how long your average showers are:")
+print("I take " + avg_per_showers + " mins to shower") # I take 20 mins to shower
+# Stage 2
+Total_water_
